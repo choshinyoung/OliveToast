0ff93c3e21639d26d38422cc0c7cd1199e97e4be
choshinyoung <50575654+choshinyoung@users.noreply.github.com>
2021-04-21 17:13:46 +0900

    초기 커밋.

---
+++ OliveToast/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OliveToast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
