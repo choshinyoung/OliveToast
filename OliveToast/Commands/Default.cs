eb9aa5ef6125836d8601898fde3117c17f44f5ae
choshinyoung <50575654+choshinyoung@users.noreply.github.com>
2021-04-23 23:27:51 +0900

    feat(default): create category help command

--- OliveToast/Commands/Default.cs
+++ OliveToast/Commands/Default.cs
@@ -15,7 +15,7 @@ namespace OliveToast.Commands
         [Summary("커맨드 목록을 볼 수 있습니다.")]
         public async Task Help()
         {
-            EmbedBuilder emb = Context.CreateEmbed(title: "도움말");
+            EmbedBuilder emb = Context.CreateEmbed(title: "도움말", description: $"`{EventHandler.prefix}카테고리/커맨드`로 자세한 사용법 보기");
             
             foreach(ModuleInfo module in Program.Command.Modules)
             {
@@ -28,7 +28,7 @@ namespace OliveToast.Commands
                             cmds.Add(cmd);
                     }
 
-                    emb.AddField(EventHandler.prefix + module.Name, string.Join(' ', cmds.Select(c => $"`{EventHandler.prefix}{c.Name}`")));
+                    emb.AddField(module.Name, string.Join(' ', cmds.Select(c => $"`{EventHandler.prefix}{c.Name}`")));
                 }
             }
 
@@ -36,57 +36,79 @@ namespace OliveToast.Commands
         }
 
         [Command("도움"), Alias("도움말")]
-        [Summary("특정 커맨드의 자세한 정보를 확인힙니다.")]
-        public async Task Help([Name("커맨드"), Remainder]string cmdName)
+        [Summary("특정 카테고리 또는 커맨드의 자세한 정보를 확인합니다.")]
+        public async Task Help([Remainder, Name("카테고리/커맨드")]string name)
         {
-            List<CommandInfo> infos = Program.Command.Commands.Where(c => (c.Name == cmdName || c.Aliases.Contains(cmdName)) && c.Summary != null && !c.HaveAttribute<HideInHelp>()).ToList();
-
-            if (infos.Count == 0)
+            List<ModuleInfo> moduleInfos = Program.Command.Modules.Where(m => m.Name == name).ToList();
+            if (moduleInfos.Any())
             {
-                await Context.MsgReplyEmbedAsync("해당 커맨드는 존재하지 않습니다.");
-                return;
-            }
-
-            EmbedBuilder emb = Context.CreateEmbed();
-
-            foreach (CommandInfo info in infos)
-            {
-                string name = info.Aliases.Count > 1 ? string.Join("/", info.Aliases) : info.Name;
-
-                string param = "";
-                foreach (ParameterInfo paramInfo in info.Parameters)
+                List<CommandInfo> cmds = new();
+                foreach (CommandInfo cmd in moduleInfos.FirstOrDefault().Commands)
                 {
-                    param += $"`{paramInfo.Name}";
-
-                    Type t = paramInfo.Type;
-                    if (t == typeof(bool))
-                        param += "<bool>";
-                    else if (t == typeof(char))
-                        param += "<문자>";
-                    else if (t == typeof(byte) || t == typeof(sbyte) || t == typeof(short) || t == typeof(int) || t == typeof(long) || t == typeof(ushort) || t == typeof(uint) || t == typeof(ulong) || t == typeof(float) || t == typeof(double) || t == typeof(decimal))
-                        param += "<숫자>";
-                    else if (t == typeof(string))
-                        param += "<텍스트>";
-                    else if (t == typeof(DateTime) || t == typeof(DateTimeOffset) || t == typeof(TimeSpan))
-                        param += "<시간>";
-                    else if (typeof(Enum).IsAssignableFrom(t))
-                        param += "<enum>";
-                    else if (typeof(IUser).IsAssignableFrom(t))
-                        param += "<유저>";
-                    else if (typeof(IChannel).IsAssignableFrom(t))
-                        param += "<채널>";
-                    else if (typeof(IRole).IsAssignableFrom(t))
-                        param += "<역할>";
-                    else if (typeof(IMessage).IsAssignableFrom(t))
-                        param += "<메시지>";
-
-                    param += "` ";
+                    if (!cmd.HaveAttribute<HideInHelp>() && !cmds.Where(c => c.Name == cmd.Name).Any())
+                        cmds.Add(cmd);
                 }
 
-                emb.AddField($"{EventHandler.prefix}{name} {param}", info.Summary);
-            }
+                EmbedBuilder emb = Context.CreateEmbed(title: moduleInfos.FirstOrDefault().Name);
 
-            await Context.MsgReplyEmbedAsync(emb.Build());
+                foreach (CommandInfo info in cmds)
+                {
+                    emb.AddField($"{EventHandler.prefix}{info.Name} {string.Join(' ', info.Parameters.Select(p => $"`{p.Name}`"))}", info.Summary);
+                }
+                
+                await Context.MsgReplyEmbedAsync(emb.Build());
+            }
+            else 
+            {
+                List<CommandInfo> commandInfos = Program.Command.Commands.Where(c => c.Aliases.Contains(name) && !c.HaveAttribute<HideInHelp>()).ToList();
+                if (commandInfos.Any())
+                {
+                    EmbedBuilder emb = Context.CreateEmbed();
+
+                    foreach (CommandInfo info in commandInfos)
+                    {
+                        string cmdName = info.Aliases.Count > 1 ? string.Join("/", info.Aliases) : info.Name;
+
+                        string param = "";
+                        foreach (ParameterInfo paramInfo in info.Parameters)
+                        {
+                            param += $"`{paramInfo.Name}";
+
+                            Type t = paramInfo.Type;
+                            if (t == typeof(bool))
+                                param += "<bool>";
+                            else if (t == typeof(char))
+                                param += "<문자>";
+                            else if (t == typeof(byte) || t == typeof(sbyte) || t == typeof(short) || t == typeof(int) || t == typeof(long) || t == typeof(ushort) || t == typeof(uint) || t == typeof(ulong) || t == typeof(float) || t == typeof(double) || t == typeof(decimal))
+                                param += "<숫자>";
+                            else if (t == typeof(string))
+                                param += "<텍스트>";
+                            else if (t == typeof(DateTime) || t == typeof(DateTimeOffset) || t == typeof(TimeSpan))
+                                param += "<시간>";
+                            else if (typeof(Enum).IsAssignableFrom(t))
+                                param += "<enum>";
+                            else if (typeof(IUser).IsAssignableFrom(t))
+                                param += "<유저>";
+                            else if (typeof(IChannel).IsAssignableFrom(t))
+                                param += "<채널>";
+                            else if (typeof(IRole).IsAssignableFrom(t))
+                                param += "<역할>";
+                            else if (typeof(IMessage).IsAssignableFrom(t))
+                                param += "<메시지>";
+
+                            param += "` ";
+                        }
+
+                        emb.AddField($"{EventHandler.prefix}{cmdName} {param}", info.Summary);
+                    }
+
+                    await Context.MsgReplyEmbedAsync(emb.Build());
+                }
+                else
+                {
+                    await Context.MsgReplyEmbedAsync("해당 커맨드 또는 카테고리는 존재하지 않습니다.");
+                }
+            }
         }
 
         [Command("안녕")]
