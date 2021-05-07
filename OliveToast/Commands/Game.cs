c8e42060152cde31bc83ab33a612a42fa811e7ba
choshinyoung <50575654+choshinyoung@users.noreply.github.com>
2021-05-07 22:20:49 +0900

    feat(game): rock scissors paper

--- OliveToast/Commands/Game.cs
+++ OliveToast/Commands/Game.cs
@@ -1,4 +1,5 @@
 ﻿using Discord.Commands;
+using Discord.WebSocket;
 using OliveToast.Managements;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,26 @@ namespace OliveToast.Commands
         public async Task Dice([Name("면 수")] int count = 6)
         {
             await Context.MsgReplyEmbedAsync($"{new Random().Next(1, count + 1)}!");
+        }
+
+        public enum Rcp { 가위, 바위, 보 }
+        [Command("가위바위보")]
+        [RequirePermission(PermissionType.UseBot)]
+        [Summary("가위바위보입니다\n`가위`, `바위`, `보` 중 하나를 입력할 수 있습니다")]
+        public async Task RockScissorsPaper([Name("입력")] Rcp input)
+        {
+            switch(new Random().Next(0, 3))
+            {
+                case 0:
+                    await Context.MsgReplyEmbedAsync($"{input}! 무승부!");
+                    break;
+                case 1:
+                    await Context.MsgReplyEmbedAsync($"{(Rcp)((int)(input + 1) % 3)}! 올리브토스트 승리!");
+                    break;
+                case 2:
+                    await Context.MsgReplyEmbedAsync($"{(Rcp)((int)(input - 1 + 3) % 3)}! {Context.User.Username} 승리!");
+                    break;
+            }
         }
     }
 }
