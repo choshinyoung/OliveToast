44dae2c3746d0157321c62604b70acc2636eb1ba
choshinyoung <50575654+choshinyoung@users.noreply.github.com>
2021-05-08 13:14:08 +0900

    feat(game): Rot

--- OliveToast/Commands/Game.cs
+++ OliveToast/Commands/Game.cs
@@ -17,7 +17,7 @@ namespace OliveToast.Commands
     {
         [Command("주사위"), Alias("주사위 굴리기")]
         [RequirePermission(PermissionType.UseBot)]
-        [Summary("주사위를 굴립니다\n면 수는 생략할 수 있습니다")]
+        [Summary("주사위를 굴립니다\n`면 수`는 생략할 수 있습니다")]
         public async Task Dice([Name("면 수")] int count = 6)
         {
             await Context.MsgReplyEmbedAsync($"{new Random().Next(1, count + 1)}!");
@@ -41,6 +41,40 @@ namespace OliveToast.Commands
                     await Context.MsgReplyEmbedAsync($"{(Rcp)((int)(input - 1 + 3) % 3)}! {Context.User.Username} 승리!");
                     break;
             }
+        }
+
+        [Command("추첨")]
+        [RequirePermission(PermissionType.UseBot), RequireContext(ContextType.Guild)]
+        [Summary("주어진 유저들 중 한명을 랜덤으로 선택합니다\n`유저`를 생략하면 서버의 모든 유저 중 한명을 선택합니다")]
+        public async Task Rot([Name("유저")] params SocketGuildUser[] users)
+        {
+            if (users.Length == 0)
+            {
+                users = Context.Guild.Users.ToArray();
+            }
+            else if (users.Length < 2)
+            {
+                await Context.MsgReplyEmbedAsync("2명 이상의 유저를 선택해주세요");
+                return;
+            }
+
+            await Context.MsgReplyEmbedAsync($"축하합니다! ||{users[new Random().Next(0, users.Length)].Mention}||님이 당첨되었습니다! :tada:");
+        }
+
+        [Command("추첨")]
+        [RequirePermission(PermissionType.UseBot), RequireContext(ContextType.Guild)]
+        [Summary("주어진 역할을 가진 유저들 중 한명을 랜덤으로 선택합니다")]
+        public async Task Rot([Remainder, Name("역할")] SocketRole role)
+        {
+            SocketGuildUser[] users = Context.Guild.Users.Where(u => u.Roles.Any(r => r.Id == role.Id)).ToArray();
+
+            if (users.Length < 2)
+            {
+                await Context.MsgReplyEmbedAsync("2명 이상의 유저를 선택해주세요");
+                return;
+            }
+
+            await Rot(users);
         }
     }
 }
