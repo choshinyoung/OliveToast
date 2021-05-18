a1236fc0a02b594244a7b7f86c6201ab65954318
choshinyoung <50575654+choshinyoung@users.noreply.github.com>
2021-05-18 23:45:48 +0900

    feat(log): log types

--- OliveToast/Managements/DbManager.cs
+++ OliveToast/Managements/DbManager.cs
@@ -45,10 +45,19 @@ namespace OliveToast.Managements
         {
             public ulong? LogChannelId;
 
+            public enum LogTypes { 메시지수정, 메시지삭제, 메시지뭉탱이삭제, 채널생성, 채널삭제, 채널설정, 서버설정, 초대링크생성, 초대링크삭제, 반응추가, 반응제거, 모든반응제거, 역할생성, 역할삭제, 역할설정, 차단, 입장, 퇴장, 차단해제, 유저설정, 음성상태설정, 음성서버설정 }
+            public LogTypes LogType;
+
             public GuildSetting()
             {
                 LogChannelId = null;
+                LogType = LogTypes.메시지수정 & LogTypes.메시지삭제;
             }
         }
+    }
+
+    class OliveUser
+    {
+
     }
 }
