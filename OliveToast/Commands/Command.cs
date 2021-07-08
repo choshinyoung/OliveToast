a2bbeaee4c3739cc8bca862c83d208cf64db1a9f
choshinyoung <50575654+choshinyoung@users.noreply.github.com>
2021-07-08 16:50:26 +0900

    feat: Add Command.cs

---
+++ OliveToast/Commands/Command.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using OliveToast.Managements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OliveToast.Managements.RequireCategoryEnable;
+
+namespace OliveToast.Commands
+{
+    [Name("커맨드")]
+    [RequireCategoryEnable(CategoryType.Setting), RequireContext(ContextType.Guild)]
+    public class Command : ModuleBase<SocketCommandContext>
+    {
+        
+    }
+}
