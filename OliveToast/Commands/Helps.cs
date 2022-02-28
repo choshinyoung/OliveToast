e6f47bed2c59ec4886e8612858d4ba13e707ae4a
choshinyoung <50575654+choshinyoung@users.noreply.github.com>
2022-02-28 20:58:04 +0900

    fix: readability of help commands

--- OliveToast/Commands/Helps.cs
+++ OliveToast/Commands/Helps.cs
@@ -146,7 +146,7 @@ namespace OliveToast.Commands
                 }
                 permission = permission != null ? $"\n - 이 커맨드를 실행하려면 `<{permission}>` 권한이 필요해요" : "";
 
-                emb.AddField($"{CommandEventHandler.prefix}{info.Name} {param}", $"{aliases}\n{info.Summary}{permission}");
+                emb.AddField($"{CommandEventHandler.prefix}{info.Name} {param}", $"{aliases}\n{info.Summary}{permission}\n​");
             }
 
             await msg.ModifyAsync(m =>
@@ -233,7 +233,7 @@ namespace OliveToast.Commands
                         }
                         permission = permission != null ? $"\n - 이 커맨드를 실행하려면 `<{permission}>` 권한이 필요해요" : "";
 
-                        emb.AddField($"{CommandEventHandler.prefix}{info.Name} {param}", $"{aliases}\n{info.Summary}{permission}");
+                        emb.AddField($"{CommandEventHandler.prefix}{info.Name} {param}", $"{aliases}\n{info.Summary}{permission}\n​");
                     }
 
                     await Context.ReplyEmbedAsync(emb.Build());
